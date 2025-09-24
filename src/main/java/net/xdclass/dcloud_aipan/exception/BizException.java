aee9a136220635867ab37b6c02e2963dcbe9df70
Simoon <420526391@qq.com>
2025-09-25 07:14:49 +0800

    工具类添加

---
+++ src/main/java/net/xdclass/dcloud_aipan/exception/BizException.java
@@ -0,0 +1,30 @@
+package net.xdclass.dcloud_aipan.exception;
+
+import lombok.Data;
+import net.xdclass.dcloud_aipan.enums.BizCodeEnum;
+
+@Data
+public class BizException extends RuntimeException {
+
+    private int code;
+    private String msg;
+    private String detail;
+    public BizException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.msg = message;
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum){
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum, Exception e){
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+        this.detail = e.toString();
+    }
+}
