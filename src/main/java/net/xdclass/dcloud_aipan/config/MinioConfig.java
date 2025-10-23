f8e18b23308f36bff29c559c7f9da2ba9d01b19e
Simoon <420526391@qq.com>
2025-10-24 07:39:27 +0800

    删除问题代码

--- src/main/java/net/xdclass/dcloud_aipan/config/MinioConfig.java
+++ src/main/java/net/xdclass/dcloud_aipan/config/MinioConfig.java
@@ -2,7 +2,6 @@ package net.xdclass.dcloud_aipan.config;
 
 import io.minio.MinioClient;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
--- src/main/java/net/xdclass/dcloud_aipan/controller/TestController.java
+++ src/main/java/net/xdclass/dcloud_aipan/controller/TestController.java
@@ -13,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/api/test/v1")
