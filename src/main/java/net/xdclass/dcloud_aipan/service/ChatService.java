7626e8ca4cea14f3b74f896a198ee6faf17dacb7
Simoon <420526391@qq.com>
2026-01-27 23:23:48 +0800

    Spring AI 调用-上

---
+++ src/main/java/net/xdclass/dcloud_aipan/service/ChatService.java
@@ -0,0 +1,4 @@
+package net.xdclass.dcloud_aipan.service;
+
+public interface ChatService {
+}
---
+++ src/main/java/net/xdclass/dcloud_aipan/service/impl/ChatServiceImpl.java
@@ -0,0 +1,10 @@
+package net.xdclass.dcloud_aipan.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.dcloud_aipan.service.ChatService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ChatServiceImpl implements ChatService {
+}
